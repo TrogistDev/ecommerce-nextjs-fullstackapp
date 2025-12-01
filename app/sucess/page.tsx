f8b2be44c8bc81e9f79fccc920e5a2b8fabe6bf0
trogist15496-del <trogist15496@gmail.com>
2025-12-01 17:20:37 +0100

    redirection fixing

--- app/sucess/page.tsx
+++ app/sucess/page.tsx
@@ -9,6 +9,6 @@ export default function SucessPage(){
     useEffect(() => {clearCart()}, [clearCart])
     return <div className="container mx-auto px-4 py-8 text-center">
         <h1 className="text-3xl font-bold mb-4">Payment is Done!</h1>
-        <Link href="/products" className="text-blue-600 hover:underline">Continue Shopping</Link>
+        <Link href="https://ecommerce-nextjs-fullstackapp.vercel.app/" className="text-blue-600 hover:underline">Continue Shopping</Link>
     </div>
 }
